bc901b063d6d7f0a3614eda0bf3300fc91a03e1d
victor <nyakundivictorn@gmail.com>
2024-05-13 19:24:48 +0300

    refining ui

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -7,7 +7,7 @@ const Service = () => {
         <h2 className="text-lg md:text-3xl font-bold text-gray-600 mb-4">
           Discover What I Offer
         </h2>
-        <p className="text-sm md:text-base text-gray-600 mb-6">
+        <p className="text-sm md:text-base text-gray-600 mb-4">
           
           Ready to turn your ideas into reality? Let&apos;s work together to bring
           your projects to life.
